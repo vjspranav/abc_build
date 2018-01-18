43b1c39b6d08043c003e018649cb2bc2e206d221
vjspranav <pranavasri@live.in>
2018-01-18 20:06:36 +0000

    Trying to fix parse errors

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -94,9 +94,9 @@ $(SOONG_VARIABLES): FORCE
 	echo '    "DeviceKernelHeaders": $(call json_list,$(strip $(TARGET_PROJECT_SYSTEM_INCLUDES))),'; \
         echo '    "TargetUsesQCOMBsp":  $(if $(TARGET_USES_QCOM_BSP),true,false),';  \
 	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),'; \
-	echo '    "QTIAudioPath":  $"(call project-path-for,qcom-audio)",'; \
-	echo '    "QTIDisplayPath":  $"(call project-path-for,qcom-display)",'; \
-	echo '    "QTIMediaPath":  $"(call project-path-for,qcom-media)",';  \
+	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
+	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
+	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)",';  \
         echo '}') > $(SOONG_VARIABLES_TMP); \
 	if ! cmp -s $(SOONG_VARIABLES_TMP) $(SOONG_VARIABLES); then \
 	  mv $(SOONG_VARIABLES_TMP) $(SOONG_VARIABLES); \
